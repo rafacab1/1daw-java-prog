03d7ed3af043ddce172fb6200e90078ae4dee13b
Rafa Caballero <rafacab.dev@gmail.com>
2020-02-04 16:27:48 +0100

    Clase Fecha

--- segundoTrimestre/POO/fecha/Fecha.java
+++ segundoTrimestre/POO/fecha/Fecha.java
@@ -46,6 +46,67 @@ public class Fecha {
     }
   }
   
+  public Fecha suma1Dia() {
+    int dia = this.dia +1;
+    int mes = this.mes;
+    int anyo = this.anyo;
+    
+    if (dia>this.diasMes(mes, anyo)) {
+      dia = 1;
+      mes++; // Paso al mes siguiente
+      if (mes>12) { 
+        mes = 1; // Paso al año siguiente
+        anyo++;
+      }
+    }
+    Fecha tmp = new Fecha(dia, mes, anyo);
+    return tmp;
+  }
+  
+  public Fecha resta1Dia() {
+    int dia = this.dia - 1;
+    int mes = this.mes;
+    int anyo = this.anyo;
+    if (dia==0) {
+      mes--; // Paso al mes anterior
+      if (mes==0) {
+        mes = 12;
+        anyo--;
+      }
+      dia = this.diasMes(mes, anyo);
+    }
+    Fecha tmp = new Fecha(dia, mes, anyo);
+    return tmp;
+  }
+  
+  public Fecha sumaNDias(int value) {
+    Fecha tmp = this;
+    if (value>0) {
+      for (int i=1;i<=value;i++) {
+        tmp = tmp.suma1Dia();
+      }
+    } else {
+      for (int i=1;i<=Math.abs(value);i++) {
+        tmp = tmp.suma1Dia();
+      }
+    }
+    return tmp;
+  }
+  
+  public Fecha restaNDias(int value) {
+    Fecha tmp = this;
+    if (value>0) {
+      for (int i=1;i<=value;i++) {
+        tmp = tmp.resta1Dia();
+      }
+    } else {
+      for (int i=1;i<=Math.abs(value);i++) {
+        tmp = tmp.resta1Dia();
+      }
+    }
+    return tmp;
+  }
+  
   public boolean esCorrecta(int dia, int mes, int anyo) {
     // Comprobar si el año es correcto
     if (anyo<0) {
@@ -58,17 +119,29 @@ public class Fecha {
     }
     
     // Comprobar si el día es correcto
+    if (dia > 0 && dia <= diasMes(this.mes, this.anyo)) {
+      return true;
+    } else {
+      return false;
+    } 
+  }
+  
+  public String nombreMes() {
+    String[] meses = {"enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "noviembre", "diciembre"};
+    return meses[this.mes-1];
+  }
+  
+  public int diasMes(int mes, int anyo) {
     int[] diasMes = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     if (this.esBisiesto(anyo)) {
       diasMes[1] = 29;
     }
-    if (dia > 0 && dia <= diasMes[mes-1]) {
-      return true;
-    } else {
-      return false;
-    }
-    
+    return diasMes[mes-1];
   }
   
+  public String toString() {
+    String str = this.dia + " de " + this.nombreMes() + " de " + this.anyo;
+    return str;
+  }
 }
 
